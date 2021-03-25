f782da719dc8a1129bdf8dc09cff4b62875799d3
mikael <mbriolet.ma@gmail.com>
2021-03-25 11:22:59 +0100

    add p2c2

---
+++ p2c2_forum.py
@@ -0,0 +1,109 @@
+"""Définit les classes propres à notre forum. ;)"""
+
+
+class File:
+    """Fichier."""
+
+    def __init__(self, name, size):
+        """Initialise le nom et la taille."""
+        self.name = name
+        self.size = size
+
+    def display(self):
+        """Affiche le fichier."""
+        print(f"Fichier '{self.name}'.")
+
+
+class ImageFile(File):
+    """Fichier image.
+
+    Pas plus à ajouter pour l'instant !
+    """
+
+    pass
+
+
+class User:
+    """Utilisateur."""
+
+    def __init__(self, username, password):
+        """Initialise le nom d'utilisateur et le mot de passe."""
+        self.username = username
+        self.password = password
+
+    def login(self):
+        """Connecte l'utilisateur."""
+        print(f"L'utilisateur {self.username} est connecté.")
+
+    def post(self, thread, content):
+        """Poste un message dans un fil de discussion."""
+        post = Post(user=self, time_posted="aujourd'hui !", content=content)
+        thread.add_post(post)
+
+    def make_thread(self, title, content):
+        """Créé un nouveau fil de discussion."""
+        return Thread(title, "aujourd'hui !", content)
+
+
+class Moderator(User):
+    """Utilisateur modérateur."""
+
+    def edit(self, post, content):
+        """Modifie un message."""
+        post.content = content
+
+    def delete(self, thread, post):
+        """Supprime un message."""
+        index = thread.posts.index(post)
+        del thread.posts[index]
+
+
+class Post:
+    """Message."""
+
+    def __init__(self, user, time_posted, content):
+        """Initialise l'utilisateur, la date et le contenu."""
+        self.user = user
+        self.time_posted = time_posted
+        self.content = content
+
+    def display(self):
+        """Affiche le message."""
+        print(f"Message posté par {self.user} le {self.time_posted}:")
+        print(self.content)
+
+
+class FilePost(Post):
+    """Message comportant un fichier."""
+
+    def __init__(self, user, time_posted, content, file):
+        """Initialise le fichier."""
+        self.user = user
+        self.time_posted = time_posted
+        self.content = content
+        self.file = file
+
+
+class Thread:
+    """Fil de discussions."""
+
+    def __init__(self, title, time_posted, post):
+        """Initialise le titre, la date et les posts.
+
+        Attention ici: on commence par un seul post, celui du sujet.
+        Les réponses à ce post ne pourrons s'ajouter qu'ultérieurement.
+        En effet, on ne créé pas directement un nouveau fil avec des réponses. ;)
+        """
+        self.title = title
+        self.time_posted = time_posted
+        self.posts = [post]
+
+    def display(self):
+        """Affiche le fil de discussion."""
+        print(f"{self.title}, le {self.time_posted}")
+        for post in self.posts:
+            post.display()
+
+    def add_post(self, post):
+        """Ajoute un post."""
+        self.posts.append(post)
